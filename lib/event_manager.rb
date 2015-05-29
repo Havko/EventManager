199682a37b3d99a62e50cd0e92955fb9f680ca9d
Steven Havko <stevenhavko@gmail.com>
2015-05-29 14:02:10 -0400

    Eventmanager updates

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,8 +1,19 @@
+require "csv"
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, "0")[0..4]
+  end
+  
 puts "EventManager Initialized!"
 
-lines = File.readlines "event_attendees.csv"
-lines.each do |line|
-  columns = line.split(",")
-  puts columns
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  
+  
+  puts "#{name} #{zipcode}"
+  
  end
+  
  
